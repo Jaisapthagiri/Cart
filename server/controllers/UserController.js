98c128d430162dbf166fc9067e35534ca75dcaca
Jaisapthagiri <imjai6504@email.com>
2025-07-12 18:35:33 +0530

    corrected small error

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -61,7 +61,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -81,7 +81,7 @@ export const isAuth = async (req, res) => {
         return res.json({ success: true, user })
     } catch (error) {
         console.log(error.message);
-        res.json({ succes: false, message: error.message })
+        res.json({ success: false, message: error.message })
     }
 }
 
