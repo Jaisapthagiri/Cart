f64260c505b2b4d5d3a1aa387344909de9112815
Jaisapthagiri <imjai6504@email.com>
2025-07-13 07:17:30 +0530

    changed port no

--- server/server.js
+++ server/server.js
@@ -33,6 +33,6 @@ app.use('/api/cart',cartRouter)
 app.use('/api/address' , addressRouter)
 app.use('/api/order' , orderRouter)
 
-app.listen(PORT , () => {
-    console.log(`API is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`API is running on http://0.0.0.0:${PORT}`);
 })
